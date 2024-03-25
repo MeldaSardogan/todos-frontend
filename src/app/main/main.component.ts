12a3cfcf690799db6665b0aea1513d0aa2e77897
Melda <meldaa3801@gmail.com>
2024-03-25 16:47:18 +0100

    FE Design & BE

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TodoServiceService} from "../shared/todo-service.service";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  tasks: any;
 
+  constructor(private todoService: TodoServiceService) {
+  }
+
+  ngOnInit(): void {
+    this.tasks = this.todoService.getTasks();
+    console.log(this.tasks)
+  }
 }
